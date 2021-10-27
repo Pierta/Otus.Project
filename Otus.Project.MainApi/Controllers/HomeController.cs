7490844510728115743b231f72d7f98d105af379
Nikita Bestuzhev <nikita.bestuzhev@bookedby.com>
2021-10-27 12:49:59 +0300

    2nd commit: * dummy API with /health endpoint * dockerfile * k8s manifests

---
+++ Otus.Project.MainApi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Otus.Project.MainApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{param}")]
+        public IActionResult Get([FromRoute] string param)
+        {
+            _logger.LogInformation($"GET '/Home' with param = '{param}' has been requested");
+            
+            return Ok($"{param}");
+        }
+    }
+}
