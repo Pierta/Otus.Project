677433114463bf8f4ef3c819dfa3a711bd939018
Nikita Bestuzhev <nikita.bestuzhev@bookedby.com>
2022-01-16 21:08:04 +0300

    Added dummy Notification Api.

---
+++ Otus.Project.NotificationApi/Controllers/NotificationsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Otus.Project.NotificationApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly ILogger<NotificationsController> _logger;
+
+        public NotificationsController(ILogger<NotificationsController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
---
+++ Otus.Project.NotificationApi/Settings/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Otus.Project.NotificationApi.Settings
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
