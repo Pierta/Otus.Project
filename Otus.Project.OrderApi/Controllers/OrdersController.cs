bf6bfd326c470f83db755748168bee3c510aeaf8
Nikita Bestuzhev <nikita.bestuzhev@bookedby.com>
2022-01-16 20:19:14 +0300

    Added dummy Order API.

---
+++ Otus.Project.OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Otus.Project.OrderApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
---
+++ Otus.Project.OrderApi/Settings/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Otus.Project.OrderApi.Settings
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
